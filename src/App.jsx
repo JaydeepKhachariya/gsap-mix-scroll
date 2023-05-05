d001fae25d3c50fb340f1000d1b974284a5ff75f
Jaydeep <khachariyajaydeep@gmail.com>
2023-05-05 23:40:56 +0530

    Final commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import "./App.css";
+import Hero from "./components/Hero";
+import ScrollSection from "./components/ScrollSection";
+
+function App() {
+  return (
+    <>
+      <Hero />
+      <ScrollSection />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Hero.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Hero = () => {
+  return (
+    <header className="hero__header">
+      <h1>John Doe</h1>
+      <h2>Frontend developer</h2>
+    </header>
+  );
+};
+
+export default Hero;
